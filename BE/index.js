fa3934f6c539e9a0eb52737d072e0379638986c2
Khyati Gupta <160642295+khyatig0206@users.noreply.github.com>
2025-05-25 19:22:44 +0530

    Update index.js

--- BE/index.js
+++ BE/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require("cors");
-const { connectDB } = require('./configs/db');
+const { connectDB } = require('../configs/db');
 const path = require('path');
-const adminroutes = require("./routes/adminroutes");
-const testimonialRoutes = require("./routes/testimonialRoutes");
+const adminroutes = require("../routes/adminroutes");
+const testimonialRoutes = require("../routes/testimonialRoutes");
+const serverless = require('serverless-http');
 
 dotenv.config();
 
@@ -12,16 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.use('/api/admin', adminroutes);
 app.use('/api/testimonial', testimonialRoutes);
 
-const PORT = process.env.PORT || 5000;
-
 (async () => {
   await connectDB();
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on ${PORT}`);
-  });
 })();
+
+module.exports.handler = serverless(app);
