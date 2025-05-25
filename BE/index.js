a59b6f1fc8be4fd84c0d5f37a8527dcb1966eb04
khyatig0206 <khyatig0206@gmail.com>
2025-05-25 22:07:27 +0530

    finally for render

--- BE/index.js
+++ BE/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require("cors");
-const { connectDB } = require('../configs/db');
+const { connectDB } = require('./configs/db');
 const path = require('path');
-const adminroutes = require("../routes/adminroutes");
-const testimonialRoutes = require("../routes/testimonialRoutes");
-const serverless = require('serverless-http');
+const adminroutes = require("./routes/adminroutes");
+const testimonialRoutes = require("./routes/testimonialRoutes");
 
 dotenv.config();
 
@@ -17,8 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/admin', adminroutes);
 app.use('/api/testimonial', testimonialRoutes);
 
-(async () => {
-  await connectDB();
-})();
 
-module.exports = serverless(app);
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+});
