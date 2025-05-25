e6187abf7795e458357d0a698a73d8a9dd3d075c
khyatig0206 <khyatig0206@gmail.com>
2025-05-25 19:45:05 +0530

    ready

--- BE/configs/db.js
+++ BE/configs/db.js
@@ -1,12 +1,8 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected.");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
